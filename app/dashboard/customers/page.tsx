3b341ac9a86535d1ce088a175cd3c49fae98c44f
Yu_ki <yu_ki1309@outlook.jp>
2026-03-27 14:44:53 +0900

    chapter4 レイアウトとページ作成終了

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page (){
+  return (
+    <p className="text-violet-500">Customers Page</p>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page (){
+  return (
+    <p className="text-blue-500">invoices page</p>
+  )
+}
