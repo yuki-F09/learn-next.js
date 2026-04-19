4e397e2683b42bf658387ea9e911fc8ad4402b05
Yu_ki <yu_ki1309@outlook.jp>
2026-04-19 19:04:00 +0900

    14章の学習終了

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,9 @@ import { revalidatePath } from 'next/cache';
 // redirect用 キャッシュをクリアしてリクエストの送信
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -102,4 +105,25 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
   //throw new Error('Failed to Delete Invoice');
+}
+
+
+ 
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
